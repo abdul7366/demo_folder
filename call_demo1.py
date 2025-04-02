2d26c7abd119ea27844f9635351db4ade72c3354
abdul7366 <abdulquadir3801@gmail.com>
2025-04-03 01:29:31 +0530

    commited

--- call_demo1.py
+++ call_demo1.py
@@ -1,7 +1,5 @@
 print("Hello Everyone")
 print("git hub")
-<<<<<<< HEAD
 print("change by remote(local)")
-=======
 print("change by git repo")
->>>>>>> 80f1be103beb7f7133c05041d98d25e0ff891e1e
+print("line 5 in local")
